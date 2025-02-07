b3b9fe37d7836e5ff4f2a8969a9efda9e6fb95a2
MeetBagda <bagdameet@gmail.com>
2025-02-07 10:29:43 +0530

    add: DB Schema Types added

---
+++ backend/types.js
@@ -0,0 +1,21 @@
+const z = require("zod");
+
+const userSchemaTypes = z.object({
+  email: z.string().email(),
+  username: z.string().min(3, "Username must be at least 3 character"),
+  password: z.string().min(4, "Password must be at least 4 character"),
+});
+
+const eventSchemaTypes = z.object({
+    title: z.string(),
+    description: z.string(),
+    startTime: z.date(),
+    endTime: z.date(),
+    category: z.string(),
+    location: z.string(),
+    organizer: z.string(),
+    attendees: z.array(z.string()),
+    maxAttendees: z.number(),
+    imageUrl: z.string(),
+    price: z.number(),
+});
